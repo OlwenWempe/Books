a3eeb0af558c6511645a91dd8dbb7bf9e226a7a7
Olwen WEMPE <wempe.olwen@gmail.com>
2025-11-20 19:00:50 +0100

    cleaning up code, added comments and initiailized caching

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use Exception;
 use App\Entity\Book;
-use App\Entity\Author;
 use App\Repository\BookRepository;
 use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -12,35 +10,57 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Validator\Constraints\Valid;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 final class BookController extends AbstractController
 {
+    /**
+     * This method retrieves a paginated list of books. It adds caching to optimize performance.
+     *
+     * @param BookRepository $bookRepository
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @param TagAwareCacheInterface $cache
+     * @return JsonResponse
+     */
     #[Route('/api/books', name: 'book', methods: ['GET'])]
     public function getBookList(
         BookRepository $bookRepository,
         SerializerInterface $serializer,
-        Request $request
+        Request $request,
+        TagAwareCacheInterface $cache
     ): JsonResponse {
         $page = $request->get('page', 1);
         $limit = $request->get('limit', 3);
 
-        $bookList = $bookRepository->findAllWithPagination($page, $limit);
+        $idCache = "getAllbooks-" . $page . "-" . $limit;
 
-        $jsonBookList = $serializer->serialize($bookList, 'json', ['groups' => 'getBooks']);
+        $jsonBookList = $cache->get($idCache, function (ItemInterface $item) use ($bookRepository, $page, $limit, $serializer) {
+            echo ("Fetching data from database...\n");
+            $item->tag("booksCache");
+            $bookList = $bookRepository->findAllWithPagination($page, $limit);
+
+            return $serializer->serialize($bookList, 'json', ['groups' => 'getBooks']);
+        });
 
         return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
     }
 
+    /**
+     * This method retrieves a book by its ID.
+     * 
+     * @param Book $book The book to retrieve
+     * @param SerializerInterface $serializer The serializer interface
+     * 
+     */
     #[Route('/api/books/{id}', name: 'detailBook', methods: ['GET'])]
-
     public function getDetailBook(Book $book, SerializerInterface $serializer): JsonResponse
     {
         $jsonBook = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
@@ -48,16 +68,36 @@ final class BookController extends AbstractController
         return new JsonResponse($jsonBook, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 
+    /**
+     * This method deletes a book according to its ID.
+     * 
+     * @param Book $book The book to delete
+     * @param EntityManagerInterface $em The entity manager
+     * @param TagAwareCacheInterface $cache The cache interface
+     * @return JsonResponse A JSON response with no content
+     */
     #[Route('/api/books/{id}', name: 'deleteBook', methods: ['DELETE'])]
-
-    public function deleteBook(Book $book, EntityManagerInterface $em): JsonResponse
+    #[IsGranted('ROLE_ADMIN', message: "You don't have access to this resource.")]
+    public function deleteBook(Book $book, EntityManagerInterface $em, TagAwareCacheInterface $cache): JsonResponse
     {
+        $cache->invalidateTags(["booksCache"]);
         $em->remove($book);
         $em->flush();
 
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
+    /**
+     * This method creates a new book. Only users with the ROLE_ADMIN role can access this endpoint.
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $em
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param AuthorRepository $authorRepository
+     * @param ValidatorInterface $validator
+     * @return JsonResponse
+     */
     #[Route('api/books', name: "createBook", methods: ['POST'])]
     #[IsGranted('ROLE_ADMIN', message: "You don't have access to this resource.")]
     public function createBook(
@@ -70,12 +110,12 @@ final class BookController extends AbstractController
     ): JsonResponse {
         $book = $serializer->deserialize($request->getContent(), Book::class, 'json');
 
-        // Récupération de l'ensemble des données envoyées sous forme de tableau
+        // Retrieves all data sent as an array
         $content = $request->toArray();
         $idAuthor = $content['idAuthor'] ?? -1;
 
-        // On cherche l'auteur qui correspond et on l'assigne au livre.
-        // Si "find" ne trouve pas l'auteur, alors null sera retourné.
+        // We look for the corresponding author and assign it to the book.
+        // If "find" does not find the author, then null will be returned.
 
         $author = $authorRepository->find($idAuthor);
         $book->setAuthor($author);
@@ -85,7 +125,7 @@ final class BookController extends AbstractController
             return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
         }
 
-        // envoi des données en base de données
+        // Sending data to the database
         $em->persist($book);
         $em->flush();
 
@@ -96,8 +136,18 @@ final class BookController extends AbstractController
         return new JsonResponse($jsonBook, Response::HTTP_CREATED, ["location" => $location], true);
     }
 
+    /**
+     * This method updates an existing book. Only users with the ROLE_ADMIN role can access this endpoint.
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param AuthorRepository $authorRepository
+     * @param Book $book
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
     #[Route('/api/books/{id}', name: 'updateBook', methods: ['PUT'])]
-
+    #[IsGranted('ROLE_ADMIN', message: "You don't have access to this resource.")]
     public function updateBook(
         Request $request,
         EntityManagerInterface $em,
@@ -107,15 +157,15 @@ final class BookController extends AbstractController
     ): JsonResponse {
         $updatedBook = $serializer->deserialize($request->getContent(), Book::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $book]);
 
-        // Récupération de l'ensemble des données envoyées sous forme de tableau
+        // Retrieves all data sent as an array
         $content = $request->toArray();
-        // On récupère l'id de l'auteur
+        // We retrieve the author's id
         $idAuthor = $content['idAuthor'] ?? -1;
-        // On cherche l'auteur qui correspond et on l'assigne au livre.
-        // Si "find" ne trouve pas l'auteur, alors null sera retourné.
+        // We look for the corresponding author and assign it to the book.
+        // If "find" does not find the author, then null will be returned.
         $updatedBook->setAuthor($authorRepository->find($idAuthor));
 
-        // envoi des données en base de données
+        // Sending data to the database
         $em->persist($updatedBook);
         $em->flush();
 
